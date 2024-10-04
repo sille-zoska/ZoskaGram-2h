8baf08f0e1a3a32f84b0d92a4297ed91cd04cdaa
sille-zoska <sille@zochova.sk>
2024-10-04 13:45:11 +0200

    dynamic routes

--- src/app/prispevok/[prispevokId]/komentar/page.tsx
+++ src/app/prispevok/[prispevokId]/komentar/page.tsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 
 export const metadata = { title: "Zoznam komentárov | ZoškaSnap" };
 
-export default function CommentsList() {
+export default function PostCommentsList() {
 
   return (
 
